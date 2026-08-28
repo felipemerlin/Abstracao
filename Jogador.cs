158edc9e3c64555e8ab3fcf880f83f39c8765e40
felipemerlin <felipe.simao@edu.senai.br>
2026-08-28 11:13:38 -0300

    Atividade4

---
+++ Jogador.cs
@@ -0,0 +1,18 @@
+﻿namespace Abstracao
+{
+    internal class Jogador
+    {
+        //ATRIBUTOS
+        public string Nome;
+        public string Posicao;
+        public int NumCamisa;
+
+        //METODOS (COMPORTAMENTO)
+        public Jogador(string nome, string posicao, int numeroCamisa)
+        {
+            Nome = nome;
+            Posicao = posicao;
+            NumCamisa = numeroCamisa;
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,30 @@
+﻿namespace Abstracao
+{
+    internal class Time
+    {
+        //ATRIBUTOS
+        public string NomeDoTime;
+        public List<Jogador> ListaDeJogadores;
+        public Time(string nomeDoTime)
+        {
+            NomeDoTime = nomeDoTime;
+            ListaDeJogadores = new List<Jogador>();
+        }
+
+        // MÉTODOS (COMPORTAMENTO)
+        public void AdicionarJogador(Jogador jogador)
+        {
+            ListaDeJogadores.Add(jogador);
+        }
+
+        public void ListarJogadores()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Escalação da {NomeDoTime}:");
+            foreach (Jogador jogador in ListaDeJogadores)
+            {
+                Console.WriteLine($"- {jogador.Nome} | Posição: {jogador.Posicao} | Número da camisa: {jogador.NumCamisa}");
+            }
+        }
+    }
+}
