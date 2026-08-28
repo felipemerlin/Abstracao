3a3039831f42a2632ead0f745ecc72e7062e5b74
felipemerlin <felipe.simao@edu.senai.br>
2026-08-28 09:43:07 -0300

    Atividade 2

---
+++ Aluno.cs
@@ -0,0 +1,22 @@
+﻿namespace Abstracao
+{
+    public class Aluno
+    {
+        //ATRIBUTOS
+        public string Nome;
+        public int Matricula;
+        public double Nota1;
+        public double Nota2;
+
+        //METODOS (COMPORTAMENTO)
+        public void CalcularMedia()
+        {
+            double Media = (Nota1 + Nota2) / 2;
+            Console.WriteLine($"O nome do aluno é: {Nome}");
+            Console.WriteLine($"O matrícula do aluno é: {Matricula}");
+            Console.WriteLine($"A primeira nota do aluno é: {Nota1}");
+            Console.WriteLine($"A segunda nota do aluno é: {Nota2}");
+            Console.WriteLine($"A média do aluno é: {Media}");
+        }
+    }
+}
